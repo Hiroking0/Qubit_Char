e7bfbe39836f3ee21bcdd13c1a9368bd1b9e9eed
lqc <lqc@CP-1230-A-MEAS.lps.umd.edu>
2023-05-19 14:52:41 -0400

    added a bunch of qcodes for ENA measurements

---
+++ qcodes/ena_qcodes.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 19 10:16:39 2023
+
+@author: lqc
+"""
+from qcodes.instrument_drivers.Keysight.N52xx import PNABase
+import numpy as np
+from matplotlib import pyplot as plt
+
+VNA = PNABase(name = 'test',
+              address = 'TCPIP0::K-E5080B-00202.local::hislip0::INSTR',
+              min_freq = 9e6,
+              max_freq = 9e9,
+              min_power = -100,
+              max_power = 20,
+              nports = 2
+              )
+print(VNA.get_options())
+probe_start  = 4e9
+probe_stop   = 8e9
+
+probe_pwr = 0 # in dBm
+num_points=1001
+if_bandwidth=500
+timeout=100000
+
+VNA.set('power',probe_pwr)
+VNA.set('start',probe_start)
+VNA.set('stop',probe_stop)
+VNA.set('points',num_points)
+VNA.set('timeout',timeout)
+VNA.set('if_bandwidth',if_bandwidth)
+VNA.set('trace','S21')
+VNA.set('sweep_type', 'LOG')
+VNA.get_idn()
+
+
+VNA.traces.tr1.run_sweep()
+
+values=VNA.real()+1j*VNA.imaginary()
+coordinates=np.linspace(probe_start,probe_stop,num_points)
+plt.plot(coordinates, 20 * np.log10(np.abs(values)))
+
+plt.show()
+
+
---
+++ qcodes/qfreq_qpow_sweep.py
@@ -0,0 +1,93 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 19 12:16:01 2023
+
+@author: lqc
+"""
+import qcodes as qc
+from RF_qcodes import AgilentN5183A
+from qcodes.instrument_drivers.Keysight.N52xx import PNABase
+from qcodes.dataset import (
+    LinSweep,
+    Measurement,
+    dond,
+    experiments,
+    initialise_or_create_database_at,
+    load_by_run_spec,
+    load_or_create_experiment,
+    plot_dataset,
+    plot_by_id
+)
+import matplotlib.pyplot as plt
+import numpy as np
+from qcodes.parameters import Parameter
+
+
+rf = AgilentN5183A('qubit_rf', "TCPIP0::172.20.1.7::5025::SOCKET")
+rf.set('power', -22)
+rf.set('frequency', 2.9396)
+
+VNA = PNABase(name = 'test',
+              address = 'TCPIP0::K-E5080B-00202.local::hislip0::INSTR',
+              min_freq = 9e6,
+              max_freq = 9e9,
+              min_power = -100,
+              max_power = 20,
+              nports = 2
+              )
+VNA.set('power', -65)
+VNA.set('points',101)
+VNA.set('timeout',None)
+VNA.set('if_bandwidth',200)
+VNA.set('cw', 7.08434e9)
+VNA.set('trace','S21')
+VNA.set('sweep_type', 'CW')
+#VNA.get_idn()
+station = qc.Station()
+station.add_component(rf)
+station.add_component(VNA)
+pow_start = -32
+pow_stop = 15
+pow_step = 2
+
+freq_start = 2.930
+freq_stop = 2.945
+freq_step = .0004
+
+
+
+initialise_or_create_database_at("./wq_pq_sweep.db")
+
+tutorial_exp = load_or_create_experiment(
+    experiment_name="tutorial_exp",
+    sample_name="synthetic data"
+)
+
+context_meas = Measurement(exp=tutorial_exp, station=station, name='context_example')
+# Register the independent parameter...
+
+param1 = rf.power
+param2 = rf.frequency
+context_meas.register_parameter(param1)
+context_meas.register_parameter(param2)
+z = Parameter(name='z', label='Magnitude', unit='dB',
+              set_cmd=None, get_cmd=None)
+
+context_meas.register_parameter(z, setpoints = (param1, param2))
+
+with context_meas.run() as datasaver:
+    for pow_set in np.arange(pow_start, pow_stop, pow_step):
+        param1.set(pow_set)
+        print(pow_set)
+        for freq_set in np.arange(freq_start, freq_stop, freq_step):
+            print(freq_set)
+            param2.set(freq_set)
+            mag = np.average(VNA.magnitude())
+            datasaver.add_result((param1, pow_set), (param2, freq_set), (z, mag))
+            dataid = datasaver.run_id
+            dataset = datasaver.dataset
+    
+plot_dataset(dataset)
+
+plt.show()
+
