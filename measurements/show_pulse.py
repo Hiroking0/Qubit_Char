d7837beaf02c402bfd584ab34f5b555387cff141
lqc <lqc@CP-1230-A-MEAS.lps.umd.edu>
2023-05-19 09:40:08 -0400

    removed unused code in show pulse

--- measurements/show_pulse.py
+++ measurements/show_pulse.py
@@ -3,21 +3,12 @@ import sys
 sys.path.append("../")
 from lib import send_funcs as sf
 #import npp_funcs as nppf
-from lib import wave_construction as be
 import yaml
-from lib import run_funcs
-
-
 
 if __name__ == "__main__":
     f = open('./general_config.yaml','r')
     params = yaml.safe_load(f)
     f.close()
-    name = params['name']
-    pattern_repeat = params['pattern_repeat']
-    zero_length = params['zero_length']
-    zero_multiple = params['zero_multiple']
-    readout_trigger_offset = params['readout_trigger_offset']
     decimation = params['decimation']
 
     pg = sf.get_pg(params)
