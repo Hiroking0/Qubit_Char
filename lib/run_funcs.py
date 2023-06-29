8bf10d258f239677f6945e3be23de288113e6c7c
lqc <lqc@CP-1230-A-MEAS.lps.umd.edu>
2023-06-29 11:50:20 -0400

    added new function for getting func call and instrument

--- lib/run_funcs.py
+++ lib/run_funcs.py
@@ -95,6 +95,52 @@ def run_and_acquire(awg,
 #other instrument(s) will not be changed
 
 
+
+def get_func_call(rm, sweep_param):
+    qubit_addr = "TCPIP0::172.20.1.7::5025::SOCKET"
+    readout_addr = "TCPIP0::172.20.1.8::5025::SOCKET"
+    q_atten_addr = "TCPIP0::172.20.1.6::5025::SOCKET"
+    r_atten_addr = "TCPIP0::172.20.1.9::5025::SOCKET"
+    twpa_addr = "TCPIP0::172.20.1.11::5025::SOCKET"
+    
+    addr_table = {
+        'wr': readout_addr,
+        'wq': qubit_addr,
+        'pr': readout_addr,
+        'pq': qubit_addr,
+        'r_att': r_atten_addr,
+        'q_att': q_atten_addr,
+        'p_twpa': twpa_addr,
+        'w_twpa': twpa_addr
+    }
+    
+    inst_table = {
+        'wr': RF.RF_source,
+        'wq': RF.RF_source,
+        'pq': RF.RF_source,
+        'pr': RF.RF_source,
+        'r_att': ATT.Atten,
+        'q_att': ATT.Atten,
+        }
+    inst_class = inst_table[sweep_param]
+    address = addr_table[sweep_param]
+    
+    inst = inst_class(rm, address)
+    
+    func_table = {
+        'wq': 'set_freq',
+        'wr': 'set_freq',
+        'pr': 'set_power',
+        'pq': 'set_power',
+        'r_att': 'set_attenuation',
+        'q_att': 'set_attenuation'
+        }
+    
+    func_call = getattr(inst, func_table[sweep_param])
+
+    return func_call
+
+
 #extra_column used by double sweep function to store second parameter.
 #It should be python list of ["parameter name", value]
 def single_sweep(name,
@@ -106,43 +152,15 @@ def single_sweep(name,
                  live_plot = False):
     #1.8 rf is for qubit
     #1.7 rf is for readout
-    rm = visa.ResourceManager()
-    qubit_addr = "TCPIP0::172.20.1.7::5025::SOCKET"
-    readout_addr = "TCPIP0::172.20.1.8::5025::SOCKET"
-    q_atten_addr = "TCPIP0::172.20.1.6::5025::SOCKET"
-    r_atten_addr = "TCPIP0::172.20.1.9::5025::SOCKET"
-    twpa_addr = "TCPIP0::172.20.1.11::5025::SOCKET"
-
-    addrs = {
-        'wr': readout_addr,
-        'wq': qubit_addr,
-        'pr': readout_addr,
-        'pq': qubit_addr,
-        'r_att': r_atten_addr,
-        'q_att': q_atten_addr,
-        'p_twpa': twpa_addr,
-        'w_twpa': twpa_addr
-    }
+    
 
     sweep_param = params['p1']
     start = params['p1start']
     stop = params['p1stop']
     step = params['p1step']
-
-    if sweep_param == 'q_att' or sweep_param == 'r_att':
-        inst = ATT.Atten(rm, addrs[sweep_param])
-    else:
-        inst = RF.RF_source(rm, addrs[sweep_param])
-
-
-    if sweep_param == 'wq' or sweep_param == 'wr' or sweep_param == 'w_twpa':
-        func_call = inst.set_freq
-    elif sweep_param == 'pq' or sweep_param == 'pr' or sweep_param == 'p_twpa':
-        func_call = inst.set_power
-    else:
-        #attenuator
-        func_call = inst.set_attenuation
-
+    
+    rm = visa.ResourceManager()
+    func_call = get_func_call(rm, sweep_param)
 
     avgsA_sub = np.zeros((num_patterns, len(np.arange(start,stop,step))))
     avgsB_sub = np.zeros((num_patterns, len(np.arange(start,stop,step))))
@@ -164,8 +182,6 @@ def single_sweep(name,
         axim1 = ax1.imshow(mags_nosub, vmin=280, vmax=320)
         
         #myobj = plt.imshow(mags_nosub, vmin = 100, vmax = 400)
-        
-
 
     sweep_num = 0
     sweeps = np.arange(start, stop, step)
@@ -258,41 +274,10 @@ def double_sweep(name,
     p2stop = params['p2stop']
     p2step = params['p2step']
     
-
-    qubit_addr = "TCPIP0::172.20.1.7::5025::SOCKET"
-    readout_addr = "TCPIP0::172.20.1.8::5025::SOCKET"
-    q_atten_addr = "TCPIP0::172.20.1.6::5025::SOCKET"
-    r_atten_addr = "TCPIP0::172.20.1.9::5025::SOCKET"
-
-    addrs = {
-        'wr': readout_addr,
-        'wq': qubit_addr,
-        'pr': readout_addr,
-        'pq': qubit_addr,
-        'r_att': r_atten_addr,
-        'q_att': q_atten_addr,
-    }
-
-    if param2 == 'q_att' or param2 == 'r_att':
-        inst = ATT.Atten(rm, addrs[param2])
-    else:
-        inst = RF.RF_source(rm, addrs[param2])
-
-
-    if param2 == 'wq' or param2 == 'wr':
-        func_call = inst.set_freq
-    elif param2 == 'pq' or param2 == 'pr':
-        func_call = inst.set_power
-    else:
-        #attenuator
-        func_call = inst.set_attenuation
-        
-
-    
-    #command = ':FREQuency:CW '+ str(pstart) + "Hz"
-    #command = ":ATT "+str(pstart)+"dB"
-
     #inner loop is p2, outer loop is p1
+
+    rm = visa.ResourceManager()
+    func_call = get_func_call(rm, param2)
 
     ylen = len(np.arange(p1start, p1stop, p1step))
     xlen = len(np.arange(p2start, p2stop, p2step))
@@ -319,7 +304,6 @@ def double_sweep(name,
         func_call(new_param)
     
     
-    #name, awg, board, num_patterns, params, sweep_param, start, stop, step, avg_start, avg_length, extra_column = None, live_plot = False
     
         (avgsA_sub, avgsB_sub, avgsA_nosub, avgsB_nosub, mags_sub, mags_nosub) = single_sweep(t_name,
                                                                         awg,
