567a077faa3d2ca458992528d584dd74ce9da374
fujisath <fujisath@umd.edu>
2023-08-23 15:52:39 -0400

    comments

--- instruments/ENA_interface.py
+++ instruments/ENA_interface.py
@@ -1,57 +1,94 @@
+"""
+Hiroki Fujisato
 
-#import pyvisa as visa
-#import time
+Class for the Keysight E5080A ENA Network Analyzer
+
+"""
+# Importing the necessary base class for the instrument
 from .LAN_instrument_base import Instrument
 
 class ENA(Instrument):
 
     def set_power(self, power: float):
-        self.inst.write('SOUR1:POW '+str(power))
+        """Set the power for the ENA
 
-    #frequency in GHz
+        Args:
+            power (float): Power level in dBm
+        """
+        self.inst.write('SOUR1:POW ' + str(power))
+
     def set_freq_range(self, startfreq: str, stopfreq: str):
-        self.inst.write('SENS1:FREQ:STAR '+startfreq+'E9')
-        self.inst.write('SENS1:FREQ:STOP '+stopfreq+'E9')   
-        
-    #mode is either 'POINt' or 'SWEEP'
+        """Set the sweep frequency range in GHz
+
+        Args:
+            startfreq (str): Start frequency of the sweep
+            stopfreq (str): End frequency of the sweep
+        """
+        self.inst.write('SENS1:FREQ:STAR ' + startfreq + 'E9')
+        self.inst.write('SENS1:FREQ:STOP ' + stopfreq + 'E9')
+
+    # Set the averaging mode and number of averages
     def set_averaging(self, mode, num: int):
-        self.inst.write('SENSe1:AVERage:MODE ' +mode)
+        """Set the averaging mode and count
+
+        Args:
+            mode (str): Averaging mode ('POINt' or 'SWEEP')
+            num (int): Number of averages
+        """
+        self.inst.write('SENSe1:AVERage:MODE ' + mode)
         self.inst.write('SENSe1:AVERage:COUNt ' + str(num))
         self.inst.write('SENSe1:AVERage:STATe 1')
-        
+
     def set_bandwidth(self, bandwidth: float):
-        self.inst.write('SENS1:BAND '+str(bandwidth))          #IFBW 1kHz
-        
+        """Set the IF bandwidth
+
+        Args:
+            bandwidth (float): IF bandwidth in Hz
+        """
+        self.inst.write('SENS1:BAND ' + str(bandwidth))  # IFBW 1kHz
+
     def set_num_points(self, numpoints: int):
-        self.inst.write('SENS1:SWE:POIN '+str(numpoints))
+        """Set the number of sweep points
+
+        Args:
+            numpoints (int): Number of sweep points
+        """
+        self.inst.write('SENS1:SWE:POIN ' + str(numpoints))
         print(str(numpoints))
-    
-    #param is 'S21', 'S12', etc...
+
+    # Create a measurement for the specified parameter
     def create_measurement(self, mname, param):
-        
-        self.inst.write('CALCulate1:PARameter:DEFine:EXT "'+mname +'",'+param)
-        self.inst.write('DISPlay:WINDow1:TRACe1:FEED "' +mname +'"')
-        self.inst.write('CALC1:FORM MLOG')                    #change the format  MLINear, MLOG,PHASe,UPH,IMAG,REAL,POLar,SMITh,SADM,SWR,GDEL,PPH,...
-        
+        """Create a measurement setup
+
+        Args:
+            mname (str): Measurement name
+            param (str): Parameter name ('S21', 'S12', etc.)
+        """
+        self.inst.write('CALCulate1:PARameter:DEFine:EXT "' + mname + '",' + param)
+        self.inst.write('DISPlay:WINDow1:TRACe1:FEED "' + mname + '"')
+        self.inst.write('CALC1:FORM MLOG')  # Change the format to MLINear, MLOG, PHASe, etc.
+
     def run_sweep(self):
-        OPC=self.inst.query('SENS:SWE:MODE SING;*OPC?')
-        #print(OPC)
-        #self.inst.write('CALC1:MEAS1:DATA:SNP:PORTs:Save "1,2", "D:/Myfile.s2p"' );
-        
-        #self.inst.write('SENS:SWE:MODE CONT')
-        self.inst.write('CALC:PAR:MNUM 1') # Selects trace number
-        #print("Ran Okay")
-        
+        """Run a single sweep and retrieve data
+
+        Returns:
+            list: List of data points from the sweep
+        """
+        OPC = self.inst.query('SENS:SWE:MODE SING;*OPC?')
+        self.inst.write('CALC:PAR:MNUM 1')  # Selects trace number
         self.inst.write("CALC:DATA? SDATA")
-        output =  self.inst.read().split(',')
-        
+        output = self.inst.read().split(',')
         return output
-        
+
     def preset(self):
+        """Display the sweep on the screen"""
         self.inst.write('SYST:FPR')
         self.inst.write('DISPlay:WINDow1:STATE ON')
-    
-    
+
     def set_freq(self, freq):
+        """Set the center frequency
+
+        Args:
+            freq (float): Center frequency in GHz
+        """
         self.inst.write(f'SENS:FREQ:CW {freq} GHz')
-    
--- instruments/RF_interface.py
+++ instruments/RF_interface.py
@@ -1,21 +1,37 @@
-
-#import pyvisa as visa
+# Import the base class for the instrument
 from .LAN_instrument_base import Instrument
 
 class RF_source(Instrument):
 
-    #freq in GHz
     def set_freq(self, freq: float):
+        """
+        Set the frequency of the RF source.
+
+        Args:
+            freq (float): Frequency in GHz to be set.
+        """
         command = ':FREQuency:CW ' + str(freq) + 'GHz'
-        print(command)
+        print(command)  # Print the command for reference
         return self.inst.write(command)
 
     def set_power(self, power: float):
+        """
+        Set the output power of the RF source.
+
+        Args:
+            power (float): Power level in dBm to be set.
+        """
         command = ':POWer:AMPLitude ' + str(power) + 'DBM'
         return self.inst.write(command)
-    
+
     def enable_out(self):
+        """
+        Enable the RF output.
+        """
         self.inst.write(':OUTPut:STATe ON')
-        
+
     def disable_out(self):
+        """
+        Disable the RF output.
+        """
         self.inst.write(':OUTPut:STATe OFF')
